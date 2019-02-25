4e6fb4cb075f80b9f33f801ee52902dea917fc34
Norlander Wilson <norlander2@aol.com>
2019-02-24 19:50:07 -0500

    changes

--- LEARNINGR2.2/R/hello.R
+++ LEARNINGR2.2/R/hello.R
@@ -13,7 +13,20 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
-#FUCK YEA
+
+x_mean <-c(1,3,9,-22,22,6,7,10,15,4,25)
+my_fun <-function(x_mean){ sum(x_mean)/length(x_mean)}
+print(my_fun(x_mean))
+
+
+x_sum <-c(1:100)
+i = as.integer(readline(prompt = "Enter a number: "))
+sum_fun<- function(x_sum){x_sum (x_sum+i)}
+print(x_sum+i)
+
+
+x_sub <-c(1:100)
+i = as.integer(readline(prompt = "Enter a number: "))
+sum_fun<- function(x_sub){x_sub (x_sub-i)}
+print(x_sub-i)
+
